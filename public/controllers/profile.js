8c5be376f42c5f9ee37f6d674a06acd6b1978640
aspire <aspire@aspire1205.aspiresys.com>
2016-09-08 23:50:41 +0530

    profile updated

--- public/controllers/profile.js
+++ public/controllers/profile.js
@@ -11,6 +11,8 @@ angular.module('profileBuilder.profile', ['ngRoute'])
 "emailId": "ameya.shukla@aspiresys.com",
 "lastName": "Shukla",
 "firstName": "Ameya",
+"mobileNumber":"9898565547",
+"dateOfBirth":"21 Jan 1995",
 "__v": 1,
 "verificationReference": [
 {
@@ -18,12 +20,44 @@ angular.module('profileBuilder.profile', ['ngRoute'])
 "_id": "57d153c3cf5df443ff3766a3",
 }
 ],
-"previousExp": [],
+"linkedinCertification": [
+{
+"certificationName": "W3School",
+"certificationYear": "2016"
+}
+],
+"linkedinSkills": [
+{
+"skillName": "AngularJS"
+},
+{
+"skillName": "NodeJS"
+}
+],
+"previousExp": [
+{
+"previousCompanyName": "Aspire",
+"fromYear": "2013",
+"location": "chennai",
+"designation": "PM",
+"toYear": "2015",
+"_id": "57d152ad4147e33f599db182"
+},
+{
+"previousCompanyName": "TCS",
+"fromYear": "2011",
+"location": "Jaipur",
+"designation": "SE",
+"toYear": "2013",
+"_id": "57d152ad4147e33f599db182"
+}
+],
 "education": [
 {
 "institution": "btecdsfh",
 "fromYear": "2451",
 "cgpa": "8.2",
+"degree":"B.Tech",
 "board": "CBSE",
 "toYear": "2451",
 "_id": "57d152ad4147e33f599db182"
@@ -32,9 +66,10 @@ angular.module('profileBuilder.profile', ['ngRoute'])
 "institution": "btecdsf676h",
 "fromYear": "2451",
 "toYear": "2451",
+"degree":"B.Tech",
 "board": "CBSE",
 "cgpa": "9.0",
-"_id": "57d152b84147e33f599db183"
+"_id": "57d152b84147e33f599db183" 
 }
 ],
 "emergencyContact": []
